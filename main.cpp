fa815f07fa5597d5655e00e21953909aabb06fd1
Libra1996 <jahunglee96@gmail.com>
2021-09-14 20:18:32 -0700

    nothing.

--- main.cpp
+++ main.cpp
@@ -8,44 +8,57 @@
 using namespace std;
 
 class LexicalAnalyzer{
-    int size
+    int size;
     public:
         LexicalAnalyzer();
         vector<Token> acceptedToken;
         vector<Token> getNextToken();
+        void ReadFile();
         void printOut();
         bool isSymbol(char);
         bool isOperator(string);
         vector<char> fileBuffer;
 };
 
-int main(int syntax, char** error){
-    LexicalAnalyzer object; /*placeholder*/
-    string file;
-    ifstream ll;
-    cout << "enter name of file (like txt): ";
-    getline(cin, file);
-    ll.open(file);
-    if(ll.is_open() and !ll.fail())
-        cout << "done\n";
+void LexicalAnalyzer::ReadFile() {
+    //Scan all the character 
+    ifstream inStream;
+    char c;
+    inStream.open("testInput.txt");
+    if (!inStream.is_open() || inStream.fail()){ //Prompt error when cannot read the file
+        cout << "File is not available.\n" ;
+        exit(1);
+    }
     else
-        cout << "it didn't work\n";
+        cout << "Success\n";
     
-    return 0;
-    // this is just sample code (I think)
-    /* just to double check:
-    cin.getline(cstring, number) is for c-strings/char arrays
-    getline(cin, stringname) is for std strings
-    */
+    while(inStream.get(c)) {
+        //Push all the characters into fileBuffer vector to handle it later
+        fileBuffer.push_back(c);
+    }
+
+    inStream.close();
+
+    for(int i=0; i<fileBuffer.size(); i++) {
+        cout << fileBuffer[i];
+    }
+    cout << endl;
+}
+
+//stream input file to characters function (getNextToken)
+vector<Token> LexicalAnalyzer::getNextToken(){
+    
+    
+    return acceptedToken;
 }
 
 //Check is the input character a symbol or not
 bool LexicalAnalyzer::isSymbol(char c){
-    if (c == '{' or c == '}' or c == '(' or c == ')' or
-        c == '<' or c == '>' or c == '+' or c == '-' or
-        c == '*' or c == '/' or c == '=' or c == '!' or
-        c == '\t'or c == '\n'or c == '"' or c == ';' or
-        c == '\''or c == isspace(c)) 
+    if (c == '{' || c == '}' || c == '(' || c == ')' ||
+        c == '<' || c == '>' || c == '+' || c == '-' ||
+        c == '*' || c == '/' || c == '=' || c == '!' ||
+        c == '\t'|| c == '\n'|| c == '"' || c == ';' ||
+        c == '\''|| c == ' ') 
         return true;
     else 
         return false;
@@ -62,27 +75,6 @@ bool LexicalAnalyzer::isOperator(string s)
         return false;
 }
 
-//stream input file to characters function (getNextToken)
-vector<Token> LexicalAnalyzer::getNextToken(){
-    //Scan all the character 
-    ifstream inStream;
-    char c;
-    inStream.open("testInput.txt");
-    if (!inStream.is_open() or inStream.fail()){ //Prompt error when cannot read the file
-        cout << "File is not available.\n" ;
-        exit(1);
-    }
-    else
-        cout << "Success\n";
-    
-    while(inStream.get(c)) //Push all the characters into fileBuffer vector to handle it later
-        fileBuffer.push_back(c);
-
-    inStream.close();
-    
-    return acceptedToken;
-}
-
 void LexicalAnalyzer::printOut(){
     int vectorSize = acceptedToken.size();
     for (int i =1; i< vectorSize;i++){
@@ -90,12 +82,9 @@ void LexicalAnalyzer::printOut(){
     }
 }
 
-//Conditional Statements
-bool LexicalAnalyzer::isOperator(string s){
-    if(s == "&&" || s == "||" || s == "==" || s == "!=" || s == "<=" || s == ">=" || s == "+"  || s == "-"  || s == "*"  || s == "/"  || s == "<"  || s == ">")
-    {
-        cout << "Operator:  " << s << endl;
-        return true;
-    }else
-        return false;
+int main(int syntax, char** error){
+    LexicalAnalyzer Lex;
+    Lex.ReadFile();
+
+    return 0;
 }
