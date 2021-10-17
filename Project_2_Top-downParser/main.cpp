1abbc80db88f3d2a6192b1febe86c20551245297
asdfghjkl90 <83328215+asdfghjkl90@users.noreply.github.com>
2021-10-17 13:36:31 -0700

    Update main.cpp

--- Project_2_Top-downParser/main.cpp
+++ Project_2_Top-downParser/main.cpp
@@ -4,8 +4,15 @@
 #include <stdlib.h>
 using namespace std;
 
-int main()
-{
+int main(int argc, char* argv[]){
     ifstream fin;
     fin.open("test.txt");
+    if(fin and !fin.fail())
+        cout << "File opened successfully\n";
+    else{
+        cout << "File could not be opened. Now terminating program";
+        exit(1);
+    }
+    
+    return argc;
 }
