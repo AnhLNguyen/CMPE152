d056901074fc5def184f2f08d12cdd408658aa85
asdfghjkl90 <83328215+asdfghjkl90@users.noreply.github.com>
2021-10-20 00:25:39 -0700

    Update main.cpp

--- Project_2_Top-downParser/main.cpp
+++ Project_2_Top-downParser/main.cpp
@@ -10,8 +10,10 @@ int main(int argc, char** argv){
     fin.open("test.txt");
     if(fin and !fin.fail())
         cout << "File opened successfully\n";
-    else
+    else{
+        cout << "File could not be opened. Aborting system";
         exit(1);
+    }fin.close();
     
     return argc;
 }
